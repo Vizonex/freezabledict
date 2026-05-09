c3b9ad099661842b1bf58c1d0a2294ffd3a3d77b
Vizonex <114684698+Vizonex@users.noreply.github.com>
2026-05-09 17:16:29 -0500

    Dict capi (#5)
    * modify performance with C-API
    
    * upload wheels to github as a bonus.
    
    * bump version

--- freezabledict/_frozendict.pyx
+++ freezabledict/_frozendict.pyx
@@ -1,53 +1,64 @@
-# cython: freethreading_compatible = True
+# cython: freethreading_compatible = True, nonecheck=False
 # distutils: language = c++
 
+
 from cpython.bool cimport PyBool_FromLong
+from cpython.exc cimport PyErr_SetString
+from cpython.dict cimport PyDict_Clear, PyDict_Contains, PyDict_SetItem, PyDict_Size, PyDict_DelItem
 from libcpp.atomic cimport atomic
+from cpython.mapping cimport PyMapping_Values, PyMapping_Items, PyMapping_Keys
+cimport cython
 
 import copy
 import types
-from collections.abc import MutableMapping
+from collections.abc import MutableMapping, MappingView, Sized, KeysView as _KeysView
 
 # Some Portions of code are inspired by or copied from frozenlist to 
 # prevent reinvention of the wheel.
 
+cdef extern from "Python.h":
+    # Pythonized it.
+    object PyDict_SetDefault(object p, object key, object default)
+
+cdef object __marker = object()
+
+@cython.nonecheck(False)
 cdef class FrozenDict:
     __class_getitem__ = classmethod(types.GenericAlias)
-
-    cdef atomic[bint] _frozen
-    cdef dict _items
+    cdef:
+        atomic[bint] _frozen
+        dict _items
     
     def __init__(self, *args, **kwargs) -> None:
         self._frozen.store(False)
         self._items = dict(*args, **kwargs)
-    
+
     @property
     def frozen(self):
         return PyBool_FromLong(self._frozen.load())
 
-    cdef object _check_frozen(self):
+    cdef int _check_frozen(self) except -1:
         if self._frozen.load():
-            raise RuntimeError("Cannot modify frozen dict.")
-
-    cdef inline object _fast_len(self):
-        return len(self._items)
-
-    cpdef void freeze(self) noexcept:
+            PyErr_SetString(RuntimeError, "Cannot modify frozen dict.")
+            return -1
+        return 0
+    
+    def freeze(self):
         self._frozen.store(True)
 
     def __getitem__(self, key):
-        return self._items.__getitem__(key)
+        return self._items[key]
 
     def __setitem__(self, key, value):
         self._check_frozen()
-        self._items[key] = value
+        PyDict_SetItem(self._items, key, value)
   
     def __delitem__(self, key):
         self._check_frozen()
-        del self._items[key]
+        PyDict_DelItem(self._items, key)
 
     def __len__(self):
-        return self._fast_len()
+        return PyDict_Size(self._items)
     
     def __richcmp__(self, other, op):
         if op == 0:  # <
@@ -64,35 +75,49 @@ cdef class FrozenDict:
             return dict(self) >= other
     
     def __iter__(self):
-        return self._items.__iter__()
-    
-    def __reversed__(self):
-        return self._items.__reversed__()
-    
-    def setdefault(self, *args, **kwargs):
-        self._check_frozen()
-        return self._items.setdefault(*args, **kwargs)
+        return iter(self._items)
 
-    def get(self, *args, **kwargs):
-        return self._items.get(*args, **kwargs)  
+    def __reversed__(self):
+        return reversed(self._items)
+
+    def setdefault(self, key, default=None):
+        self._check_frozen()
+        return PyDict_SetDefault(self._items, key, default)
+
+    def get(self, key, default=None):
+        return self._items.get(key, default)
 
     def items(self):
-        return self._items.items()
+        return PyMapping_Items(self._items)
     
     def keys(self):
-        return self._items.keys()
-    
+        return PyMapping_Keys(self._items)
+
     def values(self):
-        return self._items.values()
+        return PyMapping_Values(self._items)
+
+    def popitem(self):
+        self._check_frozen()
+        return self._items.popitem()
+    
+    def pop(self, key, default=__marker):
+        self._check_frozen()
+        if default is __marker:
+            return self._items.pop(key)
+        else:
+            return self._items.pop(key, default)
 
     # we do not need multiple checks so define update to add
     # some addtional speed.
-    def update(self, *args, **kwargs):
+    @cython.boundscheck(False)
+    @cython.nonecheck(False)
+    def update(self, other=(), /, **kwds):
         self._check_frozen()
-        return self._items.update(*args, **kwargs)
+        return self._items.update(other, **kwds)
 
     def __contains__(self, item):
-        return item in self._items
+        return PyBool_FromLong(PyDict_Contains(self._items, item))
+
 
     def __hash__(self):
         if self._frozen.load():
@@ -111,6 +136,10 @@ cdef class FrozenDict:
         if self._frozen.load():
             new_dict.freeze()
         return new_dict
+    
+    def clear(self):
+        self._check_frozen()
+        PyDict_Clear(self._items)
     
     def __deepcopy__(self, memo):
         cdef FrozenDict new_dict
